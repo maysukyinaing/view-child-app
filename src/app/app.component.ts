d212900e871cbe5e81058d75c231592c6ee58087
MaySuKyiNaing <maysukyinaing@gmail.com>
2023-01-29 10:32:01 +0630

    view child

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
+import {GalleryComponent} from "./gallery/gallery.component";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'view-child-app';
+// ! = null checker
+// ? = optional
+  @ViewChild(GalleryComponent) gallery!: GalleryComponent;
+
+  addNewPicture() {
+    this.gallery?.pictures.unshift(this.gallery.generateImage())
+  }
+  removeFirstPicture() {
+    this.gallery?.pictures.shift()
+  }
 }
